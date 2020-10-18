5933e7b01dd3fb2a138b8f2201f7baf68e021037
rohansxn15 <rohansxn16@gmail.com>
2020-10-18 22:59:43 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,15 @@
+$(function() {
+
+		var person = [];
+
+		$.getJSON('db.json', function(data) {
+			$.each(data.person, function(i, f) {
+				var tblRow = "<tr>" + "<td>" + f.firstName + "</td>" + "<td>"
+						+ f.lastName + "</td>" + "<td>" + f.job + "</td>"
+						+ "<td>" + f.roll + "</td>" + "</tr>"
+				$(tblRow).appendTo("#userdata tbody");
+			});
+
+		});
+
+	});
